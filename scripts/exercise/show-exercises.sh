7f9c89d3ac88a9b1ea39f525b906aa55caf8504f
cmguido <chris.m.guido@gmail.com>
2017-11-01 12:27:08 -0400

    Fix update curl script, add show script

--- scripts/exercise/show-exercises.sh
+++ scripts/exercise/show-exercises.sh
@@ -4,13 +4,7 @@ API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/exercises"
 curl "${API}${URL_PATH}/${ID}" \
   --include \
-  --request PATCH \
-  --header "Authorization: Token token=${TOKEN}" \
-  --header "Content-Type: application/json" \
-  --data '{
-    "exercise": {
-      "date": "'"${DATE}"'"
-    }
-  }'
+  --request GET \
+  --header "Authorization: Token token=$TOKEN"
 
 echo
